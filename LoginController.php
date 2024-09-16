fa34f58eca2eada5cfcdce5381ff35202631bfbd
cinitkwon <156475605+cinitkwon@users.noreply.github.com>
2024-09-17 06:49:26 +0900

    Add files via upload

---
+++ LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view("login");
+    }
+    public function loginSubmit(Request $request)
+    {
+        // return $request->all();
+        $validateData = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6|max:12'
+
+        ]);
+        
+        $email = $request->input('email');
+        $password = $request->input('password');
+        return  'email  :' .$email . ',password : ' .$password;
+    }
+}
